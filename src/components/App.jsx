26b67c67df3bc69adf2911d99abbf7dfd6a13ea7
NikolayPutyata <nikolayputyata@gmail.com>
2024-09-13 17:15:10 +0300

    upd

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,40 +1,39 @@
-import { useEffect, useState } from 'react';
-import TodoList from './ToDoList/TodoList';
-import AddItemToList from './AddItemToList/AddItemToList';
-import { nanoid } from 'nanoid';
-import toDoList from './toDoList.json';
-import SearchToDoItem from './SearchToDoItem/SearchToDoItem';
-import './App.css';
-import LoginFormFormik from './LoginFormFormik/LoginFormFormik';
+import { useEffect, useState } from "react";
+import TodoList from "./ToDoList/TodoList";
+import AddItemToList from "./AddItemToList/AddItemToList";
+import { nanoid } from "nanoid";
+import toDoList from "./toDoList.json";
+import SearchToDoItem from "./SearchToDoItem/SearchToDoItem";
+import "./App.css";
 
 const App = () => {
   const [toDo, setToDo] = useState(() => {
-    const data = window.localStorage.getItem('data');
+    const data = window.localStorage.getItem("data");
     if (data !== null) {
       return JSON.parse(data);
     }
     return toDoList;
   });
-  const [toAdd, setToAdd] = useState('');
-  const [searchingWord, setSearchingWord] = useState('');
+  const [toAdd, setToAdd] = useState("");
+  const [searchingWord, setSearchingWord] = useState("");
 
   const handlerAdd = () => {
     if (toAdd.trim()) {
-      setToDo(prev => [...prev, { id: nanoid(), text: toAdd }]);
-      setToAdd('');
+      setToDo((prev) => [...prev, { id: nanoid(), text: toAdd }]);
+      setToAdd("");
     }
   };
 
-  const handlerDelete = idItemToDelete => {
-    setToDo(prev => prev.filter(item => item.id !== idItemToDelete));
+  const handlerDelete = (idItemToDelete) => {
+    setToDo((prev) => prev.filter((item) => item.id !== idItemToDelete));
   };
 
-  const searchingArrForWord = toDo.filter(item =>
+  const searchingArrForWord = toDo.filter((item) =>
     item.text.toLowerCase().includes(searchingWord.toLowerCase())
   );
 
   useEffect(() => {
-    window.localStorage.setItem('data', JSON.stringify(toDo));
+    window.localStorage.setItem("data", JSON.stringify(toDo));
   }, [toDo]);
 
   return (
@@ -42,7 +41,6 @@ const App = () => {
       <AddItemToList addItemFu={handlerAdd} value={toAdd} changeFu={setToAdd} />
       <SearchToDoItem value={searchingWord} onChangeFu={setSearchingWord} />
       <TodoList list={searchingArrForWord} deleteFu={handlerDelete} />
-      <LoginFormFormik />
     </>
   );
 };
